faaab5d5e067cf7a40ce13563403fa94bbeca6b3
Shumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2018-09-08 15:38:10 -0400

    Adds generator version of funciton that reads two lines from a file at a time

--- read_n.py
+++ read_n.py
@@ -6,7 +6,7 @@ def read_n(filename):
 
 def read_1(filename):
   '''
-  Generator function that reads one line from given file
+  Generator function that reads one line at a time from given file
   '''
   f = open(filename)
 
@@ -17,3 +17,19 @@ def read_1(filename):
       break
 
     yield first_line
+
+def read_2(filename):
+  '''
+  Generator function that reads two lines at a time from given file
+  '''
+  f = open(filename)
+
+  while True:
+    first_line = f.readline()
+
+    if not first_line:
+      break
+
+    second_line = f.readline()
+
+    yield first_line + second_line
