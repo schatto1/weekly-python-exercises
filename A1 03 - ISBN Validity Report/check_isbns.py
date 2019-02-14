188c0ba74c68bb0351de1ae54186e874818b5bca
Shumon Chattopadhyay <16009860+schatto1@users.noreply.github.com>
2019-02-14 11:56:05 -0500

    Renames week 3 exercise directory Removes pseudocode from week 3 code

--- A1 03 - ISBN Validity Report/check_isbns.py
+++ A1 03 - ISBN Validity Report/check_isbns.py
@@ -1,16 +1,3 @@
-'''
-Pseudocode for ISBN Checker
-1. Obtain user input
-2. Remove non-int characters from input string
-3. Make sure user input contains 13 digits
-4. Go through first 12 digits and multiply each second digit by 3
-5. Sum the first 12 digits
-6. Divide by 10, keep remainder
-7. Subtract remainder from 10
-8. Check result with checksum (13th digit)
-9. Return corresponding result
-'''
-
 # Import all necessary packages
 import re
 
